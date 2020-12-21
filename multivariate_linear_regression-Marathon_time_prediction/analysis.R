c58faa7713cc6e3ddf293590684f9b2b509593d6
andtsouris <andtsouris@gmail.com>
2020-12-21 07:37:32 +0100

    Update analysis.R

--- multivariate_linear_regression-Marathon_time_prediction/analysis.R
+++ multivariate_linear_regression-Marathon_time_prediction/analysis.R
@@ -1,9 +1,7 @@
 rm(list=ls())
 library(tidyverse)
 
-setwd('C:/Users/33767/Desktop/GitHub portfolio/Marathon times/')
-
-dat <- read.csv('./data/MarathonData.csv') %>% select(-Marathon) %>%
+dat <- read.csv('./MarathonData.csv') %>% select(-Marathon) %>%
   mutate(cycling = as.numeric(as.character(extract_numeric(CrossTraining)))) %>% 
   select(-CrossTraining, -Name, -CATEGORY, -Category) %>% 
   filter(sp4week <100)
@@ -24,14 +22,7 @@ ggplot(tested, aes(x=residuals))+
 ggplot(tested, aes(x=MarathonTime, y=predicted))+
   geom_point()
 
-#https://data.library.virginia.edu/getting-started-with-multivariate-multiple-regression/
-#https://data.library.virginia.edu/diagnostic-plots/
-
 MAE <- mean(tested$residuals) # Mean absolute error
-MAE
-accuracy10 <- nrow(filter(tested, residuals<0.1/60*100))/nrow(tested)#10 minute accuracy
-accuracy15 <- nrow(filter(tested, residuals<0.15/60*100))/nrow(tested)#15 minute accuracy
-accuracy20 <- nrow(filter(tested, residuals<0.2/60*100))/nrow(tested)#20 minute accuracy
 tested <- tested %>% mutate(ten=ifelse(residuals<0.1/60*100, yes=1, no=0),
                             fifteen=ifelse(residuals<0.15/60*100, yes=1, no=0),
                             twenty=ifelse(residuals<0.2/60*100, yes=1, no=0))
@@ -44,10 +35,7 @@ for (cur_threshold in seq(1,45)) {
 }
 
 ggplot(accuracies, aes(x=threshold, y=accuracy))+
-  #geom_point()+
   geom_smooth(span=0.2)+scale_y_continuous(breaks=seq(0,1,by=0.2))
-#at the 19 minute threshold we have the first 90% accuracy
-# the most important variable is sp4weeks prior to the race
 anova(regr)
 
 ggplot(dat2, aes(x=MarathonTime))+
@@ -56,4 +44,3 @@ ggplot(dat2, aes(x=km4week))+
   geom_density()
 ggplot(dat2, aes(x=km4week, y=MarathonTime))+
   geom_point()
-
